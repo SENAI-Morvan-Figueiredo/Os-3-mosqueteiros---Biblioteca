49c2026534ed35bbbfd00ed8a695b9ded9f40df4
Matt-ags <mateusabnelocca@gmail.com>
2025-09-25 16:54:07 -0300

    Add URL routing for 'livros' and implement AdicionarCategoria view

---
+++ BibliotecaEstrela/Livros/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "Livros"
+
+urlpatterns = [
+    path('AdicionarLivro/', views.AdicionarLivro, name='AdicionarLivro'),
+    path('AdicionarCategoria/', views.AdicionarCategoria, name='AdicionarCategoria'),
+]
--- BibliotecaEstrela/Livros/views.py
+++ BibliotecaEstrela/Livros/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Generos, Livros
+
+def AdicionarLivro(request):
+    pass
+
+def AdicionarCategoria(request):
+    generos = Generos.objects.all()
+
+    if request.method == "POST":
+        nome_genero = request.POST.get("nome_genero")
+        Generos.objects.create(
+            nome_genero=nome_genero
+        )
+        return render()
+    
+    return render(request, "AdicionarCategoria.html", {
+        "generos": generos
+    })
