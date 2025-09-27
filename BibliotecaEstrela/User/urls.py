e2b2a179f0e457f09b5c26d3f3370f45b28f935c
unknown <moraisgabriel867@gmail.com>
2025-09-27 16:11:08 -0300

    funcionalidade de editar informações do perfil está pronta, preciso apenas adicionar algumas estilizações para esconder cpf e padronizar os cpf e telefone

--- BibliotecaEstrela/User/urls.py
+++ BibliotecaEstrela/User/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('cadastro/', register, name='user_register'),
     path('tela_perfil/', tela_perfil, name='tela_perfil'),
     path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/user'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/user/login'), name='logout'),
 ]
