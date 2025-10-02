81e2e8974fab40f6602902db8198dd98d27670d4
Matt-ags <mateusabnelocca@gmail.com>
2025-10-02 16:27:13 -0300

    Adiciona página de catálogo com carrossel de livros e detalhes interativos; atualiza views e URLs

--- BibliotecaEstrela/Biblioteca/views.py
+++ BibliotecaEstrela/Biblioteca/views.py
@@ -6,3 +6,14 @@ from Livros.models import Livros
 def index(request):
     livros = Livros.objects.all()
     return render(request, 'Biblioteca/index.html', {"livros": livros})
+
+def catalogo(request):
+    livros = Livros.objects.all()
+    livros_alfabetico = Livros.objects.order_by("nome")
+    livros_disponiveis = Livros.objects.filter(status="Disponível")
+
+    return render(request, "Biblioteca/catalogo.html", {
+        "livros": livros,
+        "livros_alfabetico": livros_alfabetico,
+        "livros_disponiveis": livros_disponiveis,
+    })
