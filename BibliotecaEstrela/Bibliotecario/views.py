770ad7744fc3d658b7e758da7e64e23fb4b5bd96
Matt-ags <mateusabnelocca@gmail.com>
2025-10-09 16:56:06 -0300

    melhora layout 0 mostra empréstimos, acervo e usuários para o adm

--- BibliotecaEstrela/Bibliotecario/views.py
+++ BibliotecaEstrela/Bibliotecario/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, redirect, HttpResponse
+from Livros.models import Livros
+from User.models import Usuario
+from Biblioteca.models import Emprestimos
 
 def teste(request):
-    return HttpResponse("tela adm! - a partir do app")
+    livros = Livros.objects.all()
+    usuarios = Usuario.objects.all()
+    emprestimos = Emprestimos.objects.all()
+    context = {
+        "livros": livros,
+        "usuarios": usuarios,
+        "emprestimos": emprestimos,
+    }
+    return render(request, 'index.html', context)
