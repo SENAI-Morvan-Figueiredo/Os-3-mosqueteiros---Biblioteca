800511c1960f4c51176b09ab08d4e04b7d63546c
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-11-17 12:42:06 -0300

    login

--- BibliotecaEstrela/User/urls.py
+++ BibliotecaEstrela/User/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('cadastro/', register, name='user_register'),
     path('tela_perfil/', tela_perfil, name='tela_perfil'),
     path('historico_perfil/', historico_perfil, name='historico_perfil'),
-    path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/user/login'), name='logout'),
+    path('login/', LoginView.as_view(template_name='User/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/User/login'), name='logout'),
     path('completar_cadastro/', complete_signup, name='complete_signup'),
 ]
